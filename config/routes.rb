605b812e6b6480344a4a417b548f1c2871864351
Leandro-Pinho <leandroessencia@gmail.com>
2022-03-23 12:29:29 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get '/confirmado', action: 'confirmed', controller: 'home'
+  get '/recuperados', action: 'recovered', controller: 'home'
+  get '/mortos', action: 'deaths', controller: 'home'
+  get '/estados', action: 'countries', controller: 'home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
